6769b6a7cc290f47aa93d64570186a9f57b77896
Jack Brashier <40642637+jackmb@users.noreply.github.com>
2022-09-25 14:48:25 -0700

    Uploaded tokenless config.py

--- Scrier/config.py
+++ Scrier/config.py
@@ -1,2 +1,2 @@
-TOKEN = "OTM5Njg2Nzk4OTM2OTExODgy.Yf8dhw.m5sENvh7dJuBYTis5RjkpdgcmTE"
-INVITE_LINK = "https://discord.com/api/oauth2/authorize?client_id=939686798936911882&permissions=8&scope=bot"
+TOKEN = "Your Token Here"
+INVITE_LINK = "Your invite link here"
